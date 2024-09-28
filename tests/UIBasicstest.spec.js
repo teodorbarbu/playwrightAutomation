d39b4e0ab6ceddb3d016a27e03d291f371b749aa
teodorbarbu <teodorbarbu@yahoo.com>
2024-09-27 22:46:23 -0400

    Validate login credentials. (username and password)
    Validate signIn error message.

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,11 +1,25 @@
-const { test } = require("@playwright/test");
+const { test, expect } = require("@playwright/test");
 
-test("Browser Context Playwright test", async ({ browser }) => {
+test.only("Browser Context Playwright test", async ({ browser }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
+  const userName = page.locator("#username");
+  const signIn = page.locator("#signInBtn");
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  console.log(await page.title());
+  await page.locator("#username").type("rahulshetty");
+  await page.locator("[type='password']").type("lerning");
+  await signIn.click();
+  console.log(await page.locator("[style*='block']").textContent());
+  await expect(page.locator("[style*='block']")).toContainText("Incorrect");
+  await userName.fill("");
+  await userName.fill("rahulshettyacademy");
+  await signIn.click();
+  console.log(await page.title());
 });
 
-test.only("Page Playwrite test", async ({ page }) => {
+test("Page Playwrite test", async ({ page }) => {
   await page.goto("https://google.com");
+  console.log(await page.title());
+  await expect(page).toHaveTitle("Google");
 });
