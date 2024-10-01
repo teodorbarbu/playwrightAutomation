288048e50c5f2bfaa60f3e24081fb1758b94c03c
teodorbarbu <teodorbarbu@yahoo.com>
2024-09-30 20:53:41 -0400

    Handling Child windows & Tabs.

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -24,22 +24,47 @@ test("Browser Context Playwright test", async ({ browser }) => {
   console.log(allTitles);
 });
 
-test.only("UI Controls", async ({ page }) => {
+test("UI Controls", async ({ page }) => {
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
   const userName = page.locator("#username");
   const signIn = page.locator("#signInBtn");
+  const documentLink = page.locator("[href*='documents-request']");
   const dropdown = page.locator("select.form-control");
   await dropdown.selectOption("consult");
   await page.locator(".radiotextsty").last().click();
   await page.locator("#okayBtn").click();
 
-  //assertion to ckeck user radiobtn is selected
-
   console.log(await page.locator(".radiotextsty").last().isChecked());
+
+  //assertion to ckeck User radiobtn is selected
   await expect(page.locator(".radiotextsty").last()).toBeChecked();
   await page.locator("#terms").click();
   await expect(page.locator("#terms")).toBeChecked();
   await page.locator("#terms").uncheck();
   expect(await page.locator("#terms").isChecked()).toBeFalsy();
+  await expect(documentLink).toHaveAttribute("class", "blinkingText");
+
   //await page.pause();
+});
+
+test.only("Child Window handl", async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  const userName = page.locator("#username");
+
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  const documentLink = page.locator("[href*='documents-request']");
+
+  const [newPage] = await Promise.all([
+    context.waitForEvent("page"),
+    documentLink.click(),
+  ]);
+
+  const text = await newPage.locator(".red").textContent();
+  const arrayText = text.split("@");
+  const domain = arrayText[1].split(" ")[0];
+  console.log(domain);
+  await userName.fill(domain);
+  await page.pause();
+  console.log(await userName.textContent());
 });
