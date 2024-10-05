dd994eae0d4da8cf482ebe981ecaa69b38f61a47
teodorbarbu <teodorbarbu@yahoo.com>
2024-10-04 21:49:13 -0400

    E2E Completed.
    Dynamically find the order from OrderHistory page.

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -32,15 +32,31 @@ test("Browser Context Playwright test", async ({ page }) => {
   for (let i = 0; i < optionsCount; ++i) {
     let text = await dropdowm.locator("button").nth(i).textContent();
     if (text === " United States") {
-      dropdowm.locator("button").nth(i).click();
+      await dropdowm.locator("button").nth(i).click();
       break;
     }
   }
-  await expect(page.locator("ng-touched")).toHaveText(email);
+  await expect(page.locator(".user__name [type='text']").first()).toHaveText(
+    email
+  );
   await page.locator(".action__submit").click();
-  await expect(page.locator(".hero-primary type='text")).toHaveText(
+  await expect(page.locator(".hero-primary")).toHaveText(
     " Thankyou for the order. "
   );
-  const orderId = await page.locator(".em-spacer-1 ").textContent();
+  const orderId = await page
+    .locator(".em-spacer-1 .ng-star-inserted")
+    .textContent();
   console.log(orderId);
+  await page.locator("button[routerlink*='myorders']").click();
+  await page.locator("tbody").waitFor();
+  const rows = await page.locator("tbody tr");
+  for (let i = 0; i < (await rows.count()); ++i) {
+    const rowOrderId = await rows.nth(i).locator("th").textContent();
+    if (orderId.includes(rowOrderId)) {
+      await rows.nth(i).locator("button").first().click();
+      break;
+    }
+  }
+  const orderIdDetails = await page.locator(".col-text").textContent;
+  expect(orderId.includes(orderIdDetails)).toBeTruthy();
 });
