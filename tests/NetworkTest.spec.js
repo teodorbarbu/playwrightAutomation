62774d8acf129f4751da232ef775205d24cb487c
teodorbarbu <teodorbarbu@yahoo.com>
2024-10-22 23:19:51 -0400

    Intercepting API Response.

---
+++ tests/NetworkTest.spec.js
@@ -0,0 +1,44 @@
+const { test, expect, request } = require("@playwright/test");
+const { APIUtils } = require("./utils/APIUtils");
+
+const loginPayLoad = {
+  userEmail: "anshika@gmail.com",
+  userPassword: "Iamking@000",
+};
+const orderPayLoad = {
+  orders: [{ country: "Cuba", productOrderedId: "6581cade9fd99c85e8ee7ff5" }],
+};
+const fakePayLoadOrders = { data: [], message: "No Orders" };
+
+let response = {};
+test.beforeAll(async () => {
+  //login API
+  const apiContext = await request.newContext();
+  const apiUtils = new APIUtils(apiContext, loginPayLoad);
+  response = await apiUtils.createOrder(orderPayLoad);
+});
+
+test("WebAPIpart1/Place the order", async ({ page }) => {
+  page.addInitScript((value) => {
+    window.localStorage.setItem("token", value);
+  }, response.token);
+  await page.goto("https://rahulshettyacademy.com/client");
+  await page.route(
+    "https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/620c7bf148767f1f1215d2ca",
+    async (route) => {
+      const response = await page.request.fetch(route.request());
+      let body = fakePayLoadOrders;
+      route.fulfill({
+        response,
+        body,
+      });
+    }
+  );
+  //await page.pause();
+
+  await page.locator("button[routerlink*='myorders']").click();
+  console.log(await page.locator(".mt-4").textContent());
+
+  //await page.locator("tbody").waitFor();
+  //const rows = await page.locator("tbody tr");
+});
