268bc77bb8829437dc3dc9ae2ebe1a119ea2ca58
teodorbarbu <teodorbarbu@yahoo.com>
2024-10-25 19:36:38 -0400

    Abort Network Calls.
    Capture  Screenshots & Visual Testing.

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -18,3 +18,24 @@ test("Popup validations", async ({ page }) => {
   const textCheck = await framesPage.locator(".text h2").textContent();
   console.log(textCheck.split(" ")[1]);
 });
+
+test("Screenshot & Visual Comparison", async ({ page }) => {
+  await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+  await expect(page.locator("#displayed-text")).toBeVisible();
+  await page
+    .locator("#displayed-text")
+    .screenshot({ path: "partialScreenshot.png" });
+  await page.locator("#hide-textbox").click();
+  await page.screenshot({ path: "screenshot.png" });
+  await expect(page.locator("#displayed-text")).toBeHidden();
+});
+
+test.only("Visual Test", async ({ page }) => {
+  await page.goto("https://www.flightaware.com/");
+  expect(await page.screenshot()).toMatchSnapshot("landing.png");
+});
+
+/*test.only("Visual", async ({ page }) => {
+  await page.goto("https://www.google.com/");
+  expect(await page.screenshot()).toMatchSnapshot("homeGoogle.png");
+});*/
